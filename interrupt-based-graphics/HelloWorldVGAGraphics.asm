d94c3c63357885497c8875d17bc02ac3a9b174c7
Nick Ooms <oomsni@gmail.com>
2015-09-21 17:13:58 +0200

    Added constants

--- interrupt-based-graphics/HelloWorldVGAGraphics.asm
+++ interrupt-based-graphics/HelloWorldVGAGraphics.asm
@@ -1,50 +1,72 @@
-USE16
-ORG 0x7C00          ; boot sector
-BootStageOne
-    MOV AH, 0x00    ; reset disk
-    MOV DL, 0       ; drive number
-    INT 0x13        ; load sector
-    MOV AX, 0x0201  ; read 1 sector into memory
-    MOV DL, 0       ; drive number
-    MOV CH, 0       ; cylinder number
-    MOV DH, 0       ; head number
-    MOV CL, 2       ; starting sector number
-    MOV BX, Main    ; memory location to load to 
-    INT 0x13
-    JMP Main
+use16
 
-PadOutSector1WithZeroes ; pad out all but last 2 bytes of the sector with zeroes
-    TIMES ((0x200 - 2) - ($ - $$)) DB 0x00
-    DW 0xAA55       ; these must be last 2 bytes in the boot sector
+FLOPPY_DRIVE_NUMBER                             equ 0
 
-ORG 0x7E00          ; next sector
+FLOPPY_DRIVE_RESET_DISK_OPERATION               equ 0x00
+FLOPPY_DRIVE_RESET_DISK_INTERRUPT               equ 0x13
 
-Main
-    MOV AX, 0x0013
-    INT 0x10
-    MOV CX, 80
-    MOV DX, 50
-    JMP DrawPixel
-    DrawPixelEnd
-RET
+FLOPPY_DRIVE_READ_SECTOR_OPERATION              equ 0x02
+FLOPPY_DRIVE_READ_SECTOR_INTERRUPT              equ 0x13
 
-DrawPixel
-    MOV AX, 0x0C0F
-    INT 0x10
-    CMP CX, 240
-        JNE NextPixelX
-    CMP DX, 150
-        JNE NextPixelY
-JMP DrawPixelEnd
+FLOPPY_DRIVE_BOOT_SECTOR_ADDRESS                equ 0x7C00
+FLOPPY_DRIVE_BOOT_SECTOR_SECTOR_COUNT           equ 1
+FLOPPY_DRIVE_BOOT_SECTOR_CYLINDER_NUMBER        equ 0
+FLOPPY_DRIVE_BOOT_SECTOR_HEAD_NUMBER            equ 0
+FLOPPY_DRIVE_BOOT_SECTOR_START_SECTOR_NUMBER    equ 2
 
-NextPixelX
-    ADD CX, 1
-JMP DrawPixel
+DRAW_PIXEL_OPERATION                            equ 0x13
+DRAW_PIXEL_INTERRUPT                            equ 0x10
 
-NextPixelY
-    ADD DX, 1
-    MOV CX, 80
-JMP DrawPixel
+org FLOPPY_DRIVE_BOOT_SECTOR_ADDRESS
 
-PadOutSector2WithZeroes
-    TIMES ((0x200) - ($ - $$)) DB 0x00
+bootStageOne:
+    mov ah, FLOPPY_DRIVE_RESET_DISK_OPERATION
+    mov dl, FLOPPY_DRIVE_NUMBER
+    int     FLOPPY_DRIVE_RESET_DISK_INTERRUPT
+
+    mov ah, FLOPPY_DRIVE_READ_SECTOR_OPERATION
+    mov al, FLOPPY_DRIVE_BOOT_SECTOR_SECTOR_COUNT
+    mov dl, FLOPPY_DRIVE_NUMBER
+    mov ch, FLOPPY_DRIVE_BOOT_SECTOR_CYLINDER_NUMBER
+    mov dh, FLOPPY_DRIVE_BOOT_SECTOR_HEAD_NUMBER
+    mov cl, FLOPPY_DRIVE_BOOT_SECTOR_START_SECTOR_NUMBER
+    mov bx, main
+    int     FLOPPY_DRIVE_READ_SECTOR_INTERRUPT
+
+    jmp     main
+
+padOutSector1WithZeroes: ; pad out all but last 2 bytes of the sector with zeroes
+times ((0x200 - 2) - ($ - $$)) db 0x00
+dw 0xAA55       ; these must be last 2 bytes in the boot sector
+
+org 0x7E00          ; next sector
+
+main:
+    mov ax, DRAW_PIXEL_OPERATION
+    int     DRAW_PIXEL_INTERRUPT
+    mov cx, 80
+    mov dx, 50
+    jmp     drawPixel
+    drawPixelEnd:
+ret
+
+drawPixel:
+    mov ax, 0x0C0F
+    int     0x10
+    cmp cx, 240
+    jne     nextPixelX
+    cmp dx, 150
+    jne     nextPixelY
+jmp         drawPixelEnd
+
+nextPixelX:
+    inc cx
+jmp         drawPixel
+
+nextPixelY:
+    inc dx
+    mov cx, 80
+jmp         drawPixel
+
+padOutSector2WithZeroes:
+times ((0x200) - ($ - $$)) db 0x00
